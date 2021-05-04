ddc903a1fb51f4a510e754970f6204a62f48c43b
Johnny <jaani.matharu@gmail.com>
2021-05-04 18:33:49 -0400

    Intial installation, connecting port 3000

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+// the above code has been added by me
+
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -180,3 +186,80 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+/*
+Acceptance criteria
+
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+
+Getting Started
+The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module.
+
+The following HTML routes should be created:
+
+GET /notes should return the notes.html file.
+
+GET * should return the index.html file.
+
+The following API routes should be created:
+
+GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+Application front end must connect to an Express.js back end.
+
+Application back end must store notes with unique IDs in a JSON file.
+
+Application must be deployed to Heroku.
+
+
+
+How to Submit the Challenge
+You are required to submit BOTH of the following for review:
+
+The URL of the functional, deployed application.
+
+The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
+s
+
+Bonus: +10 Points
+Application allows users to delete notes.
+
+You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
+
+DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+
+Neil:
+HTML routes: start index.html display, call other html after start(get started), sertup server to port
+Mainly doing 4 things, all else is ready
+1. set your API routes and HTML routes
+2. write ad read DB json using fs read and write, delete (connections) using DB json and front end
+3. setting up the server
+set connetions will activate everything as css file is large, consult module
+
+Heather:
+no develop folder
+Lading page is get started,
+In develop in public this 2nd page, just create backend
+Add note , dave icon, no edit needed, just make sure save it and display back on page, pencil is reset on right, Get started will help to get started, url of deployed app is for heroic and 2nd for GitHub, notes will show in db.json, each note has unique id, to generate id, npm package, or own way, 
+*/
+
+
