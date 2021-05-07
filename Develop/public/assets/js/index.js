d2305033b7252686b65cac06d748569e2bc9eb28
Johnny <jaani.matharu@gmail.com>
2021-05-07 05:49:56 -0400

    updating main

---
+++ Develop/public/assets/js/index.js
@@ -0,0 +1,303 @@
+// the above code has been added by me
+//Check: const PORT = process.env.PORT || 3001;
+//Check middleware: app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//may also need to connect database by require database, check previou module (as its not mysql)
+
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+if (window.location.pathname === '/notes') {
+  noteTitle = document.querySelector('.note-title');
+  noteText = document.querySelector('.note-textarea');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  noteList = document.querySelectorAll('.list-container .list-group');
+}
+
+// Show an element
+const show = (elem) => {
+  elem.style.display = 'inline';
+};
+
+// Hide an element
+const hide = (elem) => {
+  elem.style.display = 'none';
+};
+
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
+
+//main GET data, the following has been added 
+
+
+//go to path and make request
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+//main POST data 
+
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+
+//main delete
+
+const deleteNote = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+const renderActiveNote = () => {
+  hide(saveNoteBtn);
+
+  if (activeNote.id) {
+    noteTitle.setAttribute('readonly', true);
+    noteText.setAttribute('readonly', true);
+    noteTitle.value = activeNote.title;
+    noteText.value = activeNote.text;
+  } else {
+    noteTitle.removeAttribute('readonly');
+    noteText.removeAttribute('readonly');
+    noteTitle.value = '';
+    noteText.value = '';
+  }
+};
+
+const handleNoteSave = () => {
+  const newNote = {
+    title: noteTitle.value,
+    text: noteText.value,
+  };
+  saveNote(newNote).then(() => {
+    getAndRenderNotes();
+    renderActiveNote();
+  });
+};
+
+// Delete the clicked note
+const handleNoteDelete = (e) => {
+  // prevents the click listener for the list from being called when the button inside of it is clicked
+  e.stopPropagation();
+
+  const note = e.target;
+  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+
+  if (activeNote.id === noteId) {
+    activeNote = {};
+  }
+
+  deleteNote(noteId).then(() => {
+    getAndRenderNotes();
+    renderActiveNote();
+  });
+};
+
+// Sets the activeNote and displays it
+const handleNoteView = (e) => {
+  e.preventDefault();
+  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+  renderActiveNote();
+};
+
+// Sets the activeNote to and empty object and allows the user to enter a new note
+const handleNewNoteView = (e) => {
+  activeNote = {};
+  renderActiveNote();
+};
+
+const handleRenderSaveBtn = () => {
+  if (!noteTitle.value.trim() || !noteText.value.trim()) {
+    hide(saveNoteBtn);
+  } else {
+    show(saveNoteBtn);
+  }
+};
+
+// Render the list of note titles
+const renderNoteList = async (notes) => {
+  let jsonNotes = await notes.json();
+  if (window.location.pathname === '/notes') {
+    noteList.forEach((el) => (el.innerHTML = ''));
+  }
+
+  let noteListItems = [];
+
+  // Returns HTML element with or without a delete button
+  const createLi = (text, delBtn = true) => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('list-group-item');
+
+    const spanEl = document.createElement('span');
+    spanEl.innerText = text;
+    spanEl.addEventListener('click', handleNoteView);
+
+    liEl.append(spanEl);
+
+    if (delBtn) {
+      const delBtnEl = document.createElement('i');
+      delBtnEl.classList.add(
+        'fas',
+        'fa-trash-alt',
+        'float-right',
+        'text-danger',
+        'delete-note'
+      );
+      delBtnEl.addEventListener('click', handleNoteDelete);
+
+      liEl.append(delBtnEl);
+    }
+
+    return liEl;
+  };
+
+  if (jsonNotes.length === 0) {
+    noteListItems.push(createLi('No saved Notes', false));
+  }
+
+  jsonNotes.forEach((note) => {
+    const li = createLi(note.title);
+    li.dataset.note = JSON.stringify(note);
+
+    noteListItems.push(li);
+  });
+
+  if (window.location.pathname === '/notes') {
+    noteListItems.forEach((note) => noteList[0].append(note));
+  }
+};
+
+// Gets notes from the db and renders them to the sidebar
+const getAndRenderNotes = () => getNotes().then(renderNoteList);
+
+if (window.location.pathname === '/notes') {
+  saveNoteBtn.addEventListener('click', handleNoteSave);
+  newNoteBtn.addEventListener('click', handleNewNoteView);
+  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+  noteText.addEventListener('keyup', handleRenderSaveBtn);
+}
+
+getAndRenderNotes();
+
+
+//added to activate the port so its listening to requests
+
+
+
+/*Possible route entry (not sure, check all sources)
+Lines
+
+7
+29
+37
+46
+122
+167
+175
+
+Module 11.1.1
+setup Heroku, make sure url deployed, make sure not to be loaded each time, if not what things need loaded each time
+History: root of assignment file -loaded express, npm install, npm start, npm init, gitignore modules (double check which ones are necessary for Express, check ["start": "node server.js"] under test on package.json )
+
+Please make sure the following
+(window.location.pathname === '/notes')
+Make sure connection of node.js with express.js to make sure no initial steps are missed
+
+Make sure we don't need server.js file, askbcs and show errors, make sure connection is there by end of day
+https://www.google.com/search?q=can+we+start+serverf+from+index.js&rlz=1C5CHFA_enCA860CA860&oq=can+we+start+serverf+from+index.js&aqs=chrome..69i57j33i10i160l2.9047j0j15&sourceid=chrome&ie=UTF-8
+Module: 12.2.4, Review the server.js File and previous chp, all should be running
+*/
+
+
+
+
+
+
+/*
+Acceptance criteria
+
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+
+Getting Started
+The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module.
+
+The following HTML routes should be created:
+
+GET /notes should return the notes.html file.
+
+GET * should return the index.html file.
+
+The following API routes should be created:
+
+GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+Application front end must connect to an Express.js back end.
+
+Application back end must store notes with unique IDs in a JSON file.
+
+Application must be deployed to Heroku.
+
+
+
+How to Submit the Challenge
+You are required to submit BOTH of the following for review:
+
+The URL of the functional, deployed application.
+
+The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
+s
+
+Bonus: +10 Points
+Application allows users to delete notes.
+
+You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
+
+DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+
+Neil:
+Mainly doing few things, all else is ready
+1. set your API routes
+2. write ad read DB json using fs read and write, delete (connections) using DB json and front end
+set connetions will activate everything as css file is large, consult module
+
+Heather:
+no develop folder
+Lading page is get started,
+In develop in public this 2nd page, just create backend
+Add note , dave icon, no edit needed, just make sure save it and display back on page, pencil is reset on right, Get started will help to get started, url of deployed app is for heroic and 2nd for GitHub, notes will show in db.json, each note has unique id, to generate id, npm package, or own way, 
+*/
---
+++ server.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = process.env.PORT || 8080;
+const { db } = require('./Develop/db/db.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Develop/public"));
+console.log(__dirname);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+//html front end routes
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  });
+
+//app.get('/api/animals', (req, res) => {
+  //res.send('Hello!');
+//});
+//this is in module and animals is the json storage file
+
+
+//GET
+app.get('/api/notes',(req,res)=>{
+  fs.readFile("./Develop/db/db.json", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+    console.log(notes);
+
+});
+});
+
+//fs.writeFile('//api//notes', (notes) =>{
+
+//}
+  //  )
+
+
+
+
+//thats how it was before 
+//app.get('/api/notes',(req,res)=>{
+//  return res.json("Hello"); 
+//}),
+
+
+//POST
+
+app.post('/api/notes',(req,res)=>{
+console.log (req.body)
+  fs.readFile("./Develop/db/db.json", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(req.body);
+console.log(notes);
+JSON.stringify(notes);
+
+  fs.writeFile("./Develop/db/db.json", data, function (err) {
+    res.json(notes);
+    if (err) {
+    console.log(err);
+    }
+});
+});
+})
+
+
+
+  
+
+
+//POST before 
+//app.post('/api/notes',(req,res)=>{
+  //res.json(req.body);
+  
+//})
+
+//DELETE
+  app.delete('/api/notes/:id', function (req, res) {
+    console.log(req.params.id);
+  
+  })
+
+
+
+
+
+
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+
+
+
+
+
+
+//https://expressjs.com/en/guide/routing.html
+//first page: http://localhost:8080, second page: http://localhost:8080/notes
+//have to insert all operation expected in notes here, check mock for demo or Monday video, show to bcs to get tips how, tutor, office hours
